4a97d00727ee5c3c7ed9c2b7b8af63b9f1e7a10e
Alyona Yanuchek <alyonayanuchek@Alyonas-MacBook-Air.local>
2025-11-27 19:21:27 -0500

    Fix build errors in SignUpModal and Index

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+
 import HeroModelCanvas from "@/components/HeroModelCanvas";
 import PlayfulCarousel from "@/components/PlayfulCarousel";
 import UploadButton from "@/components/UploadButton";
